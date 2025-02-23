a07d95c0547139a53f65e37871a189d8f646853c
JustLikeKP <kalvinpugh@gmail.com>
2025-02-23 10:13:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function showEventDetails() {
+    const details = document.getElementById("event-details");
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
 
+let slideIndex = 0;
+const images = ["path-to-image1.jpg", "path-to-image2.jpg", "path-to-image3.jpg"];
+
+function changeSlide(n) {
+    slideIndex += n;
+    if (slideIndex >= images.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = images.length - 1;
+    }
+    document.getElementById("slideshow-image").src = images[slideIndex];
+}
